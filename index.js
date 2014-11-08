5404dd8269e9c09fc9e2b95ddfd7d59bcb007e95
Nati Namvong <nati@flyingcomma.com>
2014-11-08 17:56:13 +0700

    Delete file

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var cloudinary = require('cloudinary');
 var express = require('express');
 var fs = require('fs');
 var multer = require('multer');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var format = require('util').format;
+
 var app = express();
 
 cloudinary.config({
   cloud_name: 'fitm',
   api_key: '418999862618864',
   api_secret: 'jf8C-q43_U0IovGmjg0su29kRqs'
+});
+
+var imagesDB = null;
+MongoClient.connect('mongodb://root:1234@kahana.mongohq.com:10093/bikrme', function(err, db) {
+  if (err) throw err;
+  imagesDB = db.collection('images');
 });
 
 
@@ -17,19 +27,30 @@ app.use(multer({
   dest: './uploads/'
 }));
 
+app.get('/images', function(request, res) {
+  imagesDB.find().toArray(function(err, results) {
+    if (err) res.send({
+      error: 1
+    });
+    else res.send(results);
+  });
+});
+
 app.post('/upload', function(request, response) {
   console.log(request.files.image.path);
-
-
   stream = cloudinary.uploader.upload_stream(function(result) {
-    console.log(result);
-    response.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
-      cloudinary.image(result.public_id, {
-        format: "png",
-        width: 100,
-        height: 130,
-        crop: "fill"
-      }));
+    imagesDB.insert(result, function(err, docs) {
+      if (err) response.send({
+        error: 1
+      });
+      else {
+        fs.unlink('./'+request.files.image.path, function(err) {
+          if (err) throw err;
+          console.log('successfully deleted /tmp/hello');
+        });
+        response.redirect('/index.html');
+      }
+    });
   }, {
     public_id: request.body.title
   });
@@ -37,8 +58,6 @@ app.post('/upload', function(request, response) {
   fs.createReadStream(request.files.image.path, {
     encoding: 'binary'
   }).on('data', stream.write).on('end', stream.end);
-
-
 });
 
 app.listen(app.get('port'), function() {
